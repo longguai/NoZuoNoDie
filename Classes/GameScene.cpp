2d3f906fd33d7a9359d1008aac392516a18ddb26
longguai <drs8@163.com>
2016-07-19 21:40:47 +0800

    easy set background, because sleep for me

--- Classes/GameScene.cpp
+++ Classes/GameScene.cpp
@@ -4,6 +4,10 @@
 cocos2d::Scene* GameScene::createScene()
 {
 	auto scene = Scene::create();
+	
+	// cache picture plsit
+	SpriteFrameCache::getInstance()->addSpriteFramesWithFile("hero.plist");
+
 	auto layer = GameScene::create();
 	scene->addChild(layer);
 
@@ -12,7 +16,26 @@ cocos2d::Scene* GameScene::createScene()
 
 bool GameScene::init()
 {
-	Layer::init();
+	LayerColor::initWithColor(Color4B::WHITE); // init white background
+
+	// set touch operator
+	auto listener = EventListenerTouchOneByOne::create();
+	listener->onTouchBegan = [](Touch *touch, Event *event) {
+
+		return true;
+	};
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
+
+	// create main hero sprite
+	Sprite *pHero = Hero::create();
+	_pHero = pHero;
+	addChild(_pHero);
+
+	// set schedule, use rand create block
+	scheduleUpdate();
+
+	// use store block
+
 
 	return true;
 }
--- Classes/GameScene.h
+++ Classes/GameScene.h
@@ -3,12 +3,22 @@
 #include "cocos2d.h"
 USING_NS_CC;
 
-class GameScene:public Layer
+class GameScene:public LayerColor
 {
 public:
 	static Scene* createScene();
 	CREATE_FUNC(GameScene);
 	bool init();
+
+	virtual void update(float delta);
+	void resetFrameCount();
+	Sprite *_pHero;
+
+private:
+	// use rand create block delay
+	int currentFrameIndex;
+	int nextFrameCount;
+
 };
 
 
